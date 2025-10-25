74cd83926d654e27847262846b0ff53193c5af5d
Akinola Jonathan Oyerinsola <jonalds1234567@gmail.com>
2025-10-25 12:41:27 +0100

    Update python.py

--- python.py
+++ python.py
@@ -1,6 +1,8 @@
-# Simple Python program
-# Author: Akinola Jonathan
-# ID: 24/14642
+#ASSIGNMET SEN 211
+# NAME: Akinola Jonathan
+# MATRIC NO: 24/14642
+#LEVEL : 200
+
 
 print("Hello, World!")
 print("Akinola Jonathan 24/14642")
